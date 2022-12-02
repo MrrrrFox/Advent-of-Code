5e39d8ae5e32d6f6a35d6eccd278b85523021062
Jakub Perlak <jakub.k.perlak@interia.pl>
2022-12-02 12:35:28 +0100

    Add solution 02 for AoC 2022

--- Advent of Code 2022/FileReading.py
+++ Advent of Code 2022/FileReading.py
@@ -8,3 +8,13 @@ def read_2d_int_matrix_with_group_separator(filename="data/data.txt", group_sepa
     """
     f = open(filename, 'r').read()
     return [[int(elem) for elem in row.split("\n")] for row in f.split(group_separator)]
+
+
+def read_2d_sting_matrix(filename="data/data.txt"):
+    """
+    Reading file as 2D string matrix.
+    :param filename: name of file to read
+    :return: 2D string matrix
+    """
+    f = open(filename, 'r')
+    return [[elem for elem in row.split()] for row in f]
